b1124ab18713b07bf7fb43eeb1f13dd8216c58e8
ludjoh <ludjoh734@ihmeducation.se>
2024-09-12 14:52:09 +0200

    assignment structure

---
+++ P5Operators /app2.js
@@ -0,0 +1,11 @@
+let numberofstudents = 0;
+numberofstudents++;
+console.log(numberofstudents);
+
+numberofstudents++;
+console.log(numberofstudents);
+
+numberofstudents--;
+console.log(numberofstudents);
+
+console.log(++numberofstudents);
--- P5Operators /js/app.js
+++ P5Operators /js/app.js
@@ -1,11 +1,14 @@
-let numberofstudents = 0;
-numberofstudents++;
-console.log(numberofstudents);
+let speedInput= 72;
+let speedOutput= 0;
+speedOutput=speedInput / 3.6;
+console.log(speedOutput);
 
-numberofstudents++;
-console.log(numberofstudents);
+let minutesInput= 3;
+let secondsoutput = 0;
+secondsoutput = minutesInput *60;
+console.log(secondsoutput);
 
-numberofstudents--;
-console.log(numberofstudents);
+Implement a program that takes two numbers as input and displays their division result.○Input: 11, 4○Output: 2,75
 
-console.log(++numberofstudents);
+let Input = 11,4;
+let 
