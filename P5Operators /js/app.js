8b25184c8d2ea9b9de38fff25bc1fd43cd0109a1
ludjoh <ludjoh734@ihmeducation.se>
2024-09-12 15:36:00 +0200

    finished P5

--- P5Operators /js/app.js
+++ P5Operators /js/app.js
@@ -1,14 +1,45 @@
-let speedInput= 72;
-let speedOutput= 0;
-speedOutput=speedInput / 3.6;
-console.log(speedOutput);
+//let speedInput= 72;
+//let speedOutput= 0;
+//speedOutput=speedInput / 3.6;
+//console.log(speedOutput);
 
-let minutesInput= 3;
-let secondsoutput = 0;
-secondsoutput = minutesInput *60;
-console.log(secondsoutput);
+//let minutesInput= 3;
+//let secondsoutput = 0;
+//secondsoutput = minutesInput *60;
+//console.log(secondsoutput);
 
-Implement a program that takes two numbers as input and displays their division result.○Input: 11, 4○Output: 2,75
 
-let Input = 11,4;
-let 
+//let num1 = 11;
+//let num2 = 4;
+//console.log(num1/num2);
+
+//let num1 = 11;
+//let num2 = 4;
+//console.log(num1 % num2);
+
+//let num1 = 2;
+//let num2 = 3.14;
+//console.log((num1*num1) * num2);
+
+//let num1 = 11;
+//let num2 = -1;
+//negativt = num1 * num2;
+//console.log(negativt);
+
+
+//let vikt = 70;
+//let längd = 1.82;
+//m2 = längd * längd;
+//console.log(vikt / m2);
+
+//let a = 3;
+//let b = 4;
+//let H = Math.sqrt(a*a + b*b);
+//console.log(H);
+
+let a = 111;
+let b = 60;
+console.log(1);
+console.log(a% b);
+
+
