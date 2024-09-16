ee7e2c272b8a6b8af824b9e802773150a5cae2e0
ludjoh <ludjoh734@ihmeducation.se>
2024-09-16 11:50:59 +0200

    klar uppgift P7

--- P7Boolean/js/app.js
+++ P7Boolean/js/app.js
@@ -1,16 +1,34 @@
-const birthYear = 1963;
-const currentYear = 2024;
-let ageofPerson = currentYear - birthYear;
+//const birthYear = 1963;
+//const currentYear = 2024;
+//let ageofPerson = currentYear - birthYear;
 
-let isChild = ageofPerson <= 11 && ageofPerson >=0 ;
+//let isChild = ageofPerson <= 11 && ageofPerson >=0 ;
 
-  let isTeenager = ageofPerson >= 11 && ageofPerson <= 17;
+ // let isTeenager = ageofPerson >= 11 && ageofPerson <= 17;
 
-  let isAdult = ageofPerson >= 18  ;
+//  let isAdult = ageofPerson >= 18  ;
 
-  console.log("Personen är "+ ageofPerson +" gammal");
+  //console.log("Personen är "+ ageofPerson +" gammal");
 
-  console.log("Personen är ett barn: "+ isChild+"" );
-console.log("Personen är en tonåring:  "+ isTeenager +"");
-console.log("Personen är vuxen: "+isAdult+ "");
+ // console.log("Personen är ett barn: "+ isChild+"" );
+//console.log("Personen är en tonåring:  "+ isTeenager +"");
+//console.log("Personen är vuxen: "+isAdult+ "");
+
+let playerTouchGround = false;
+
+let playerhasjetpak = false;
+
+if (playerTouchGround) {
+console.log("Player is on the ground");
+console.log("Player can jump");
+}
+
+else if (playerhasjetpak) {
+  console.log("Player cans jet away");
+}
+
+else  {
+  console.log("Player is not on the ground");
+  console.log("You can not jump");
+}
 
