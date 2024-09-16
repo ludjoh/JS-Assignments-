79ca95839b792d3617b04c83bc98771805da3cc8
ludjoh <ludjoh734@ihmeducation.se>
2024-09-16 10:24:34 +0200

    Refactor green bottles

--- P4Variables/js/app.js
+++ P4Variables/js/app.js
@@ -1,58 +1,8 @@
 let numberofBottles = 10;
-const line1 = "green bottles hanging on the wall";
-const line2 = "and if one green bottle, should accidentally fall, there will be";
+const line1 = " green bottles hanging on the wall";
+const line2 = " and if one green bottle should accidentally fall, there will be ";
 
-console.log (numberofBottles + line1);
-
-
-console.log (numberofBottles);
-console.log(line1);
-console.log (numberofBottles);
-console.log(line1);
-console.log (line2);
-console.log (--numberofBottles);
-
-console.log(line1);
-
-console.log(--numberofBottles);
-console.log(line1);
-console.log(line2);
-
-console.log(--numberofBottles);
-console.log(line1);
-console.log(line2);
+console.log ( numberofBottles  +  line1  +  line2 + --numberofBottles);
 
 
-console.log(--numberofBottles);
-console.log(line1);
-console.log(line2);
-
-
-console.log(--numberofBottles);
-console.log(line1);
-console.log(line2);
-
-
-console.log(--numberofBottles);
-console.log(line1);
-console.log(line2);
-
-
-console.log(--numberofBottles);
-console.log(line1);
-console.log(line2);
-
-
-console.log(--numberofBottles);
-console.log(line1);
-console.log(line2);
-
-
-console.log(--numberofBottles);
-console.log(line1);
-console.log(line2);
-
-
-console.log(--numberofBottles);
-console.log(line1);
 
