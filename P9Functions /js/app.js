b4b8445aba5fc0ea2916e4a075696681e244f976
ludjoh <ludjoh734@ihmeducation.se>
2024-09-16 14:57:49 +0200

    update P9

---
+++ P9Functions /js/app.js
@@ -0,0 +1,30 @@
+console.log ("Move forward");
+console.log ("Turn right");
+
+
+
+function Stepforward() {
+console.log ("Move forward");
+}
+Stepforward();
+
+function TurnRight() {
+  console.log ("TurnRight");
+}
+
+TurnRight();
+
+function TurnLeft() {
+console.log ("Turn right");
+console.log ("Turn right");
+console.log ("Turn right");
+}
+TurnLeft();
+
+function WalkThreeSteps() {
+console.log ("Move forward");
+console.log ("Move forward");
+console.log ("Move forward");
+
+}
+WalkThreeSteps()
