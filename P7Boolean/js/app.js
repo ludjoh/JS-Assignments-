b34ee851f05bb57449e4777d992678deec89af7d
ludjoh <ludjoh734@ihmeducation.se>
2024-09-16 10:59:31 +0200

    update

---
+++ P7Boolean/js/app.js
@@ -0,0 +1,14 @@
+const birthYear = 2000;
+const currentYear = 2024;
+let ageofPerson = currentYear - birthYear;
+
+let isChild = ageofPerson <= 11 ;
+
+  let isTeenager = ageofPerson >= 11 && ageofPerson <= 17;
+
+  let isAdult = ageofPerson >= 18  ;
+
+  console.log(isChild);
+console.log(isTeenager);
+console.log(isAdult);
+
