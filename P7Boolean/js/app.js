c41ba57f995ac916ce4ccf68faa2ca4afada6a47
ludjoh <ludjoh734@ihmeducation.se>
2024-09-16 11:23:59 +0200

    modified P5

--- P7Boolean/js/app.js
+++ P7Boolean/js/app.js
@@ -1,4 +1,4 @@
-const birthYear = 2000;
+const birthYear = 1963;
 const currentYear = 2024;
 let ageofPerson = currentYear - birthYear;
 
@@ -8,7 +8,9 @@ let isChild = ageofPerson <= 11 && ageofPerson >=0 ;
 
   let isAdult = ageofPerson >= 18  ;
 
-  console.log(isChild);
-console.log(isTeenager);
-console.log(isAdult);
+  console.log("Personen är "+ ageofPerson +" gammal");
+
+  console.log("Personen är ett barn: "+ isChild+"" );
+console.log("Personen är en tonåring:  "+ isTeenager +"");
+console.log("Personen är vuxen: "+isAdult+ "");
 
