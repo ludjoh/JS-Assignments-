f85d774c66daa2a58150f28f6cccb7a82c176980
ludjoh <ludjoh734@ihmeducation.se>
2024-09-17 11:01:50 +0200

    uppgifter sida 2 P10 klara

--- P10InputOutput/js/app.js
+++ P10InputOutput/js/app.js
@@ -17,9 +17,55 @@
 
 //console.log(medel(2, 6));
 
-function medel(första, andra) {
-return "Welcome" + första +  andra;
+//function medel(first, second) {
+//return "Welcome" + first +  second;
 
+//}
+
+//console.log(medel(" Ben", " Ten"));
+
+
+//function random(nummer) {
+  //return "0" or "1" or "2";
+//}
+//console.log(random(0));
+
+
+
+//function getRandomNumber() {
+  //return Math.floor(Math.random() * 3);
+//}
+//console.log(getRandomNumber());
+
+//function Skicka(nummer) {
+  //if (nummer == 0){
+  //  console.log("A");
+ // }
+ // else if (nummer == 1) {
+  //  console.log("B");
+  //}
+ // else if (nummer == 2) {
+ //   console.log("C");
+  //}
+ // return nummer;
+//}
+
+//console.log(Skicka(0));
+
+function compare(nummer1, nummer2) {
+  if (nummerA = nummer1 > nummer2) {
+    console.log( "First number is greater");
+    return nummerA;
+  }
+  if (nummerB = nummer1 < nummer2) {
+    console.log("Last number is greater");
+    return nummerB;
+  }
+   if (nummerC = nummer1 == nummer2) {
+     console.log ("Numbers are equal");
+     return nummerC;
+  }
 }
 
-console.log(medel(" Ben", " Ting"));
+console.log(compare(6 , 5));
+
