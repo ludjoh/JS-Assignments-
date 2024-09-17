04a7751e01682d5245352f9a4bbf939ae7918851
ludjoh <ludjoh734@ihmeducation.se>
2024-09-17 10:15:03 +0200

    P10 Färdiga Uppgifter

--- P10InputOutput/js/app.js
+++ P10InputOutput/js/app.js
@@ -1,11 +1,25 @@
 
-function Rufus(x) {
- return x * x;
+//function Rufus(x) {
+ //return x * x;
+//}
+
+//console.log(Rufus(4));
+
+//function adda(första, andra) {
+  //return första + andra;
+//}
+ //console.log(adda(5, 10));
+
+//function medel(första, andra) {
+  //return (första + andra) / 2;
+
+//}
+
+//console.log(medel(2, 6));
+
+function medel(första, andra) {
+return "Welcome" + första +  andra;
+
 }
 
-console.log(Rufus(4));
-
-function adda(första, andra) {
-  return första + andra;
-}
- console.log(adda(5, 10));
+console.log(medel(" Ben", " Ting"));
