781e2ce1861d42d5e75c41901bc8e19d85b18a2f
ludjoh <ludjoh734@ihmeducation.se>
2024-09-23 10:23:09 +0200

    P11 Loops uppgift klar

---
+++ P11Loops/js/app.js
@@ -0,0 +1,26 @@
+
+
+// for (let i = 0; i < 13; i++) {
+//  let randomNumber = Math.floor(Math.random() * 6);
+// console.log(randomNumber);
+//}
+
+//for (i = 1; i <= 1024; i = i * 2) {
+//console.log(i);
+//}
+
+//for (i = 10; i >= 0; i--) {
+ // console.log(i);
+//}
+
+//function power(first, second) {
+ // let result = 1;
+
+ // for (let i = 0; i < second; i++) {
+  //  result *= first;
+// }
+
+//  return result;
+//}
+
+// console.log(power(4, 24));
