7fd14f6fbdb92491b85737172db520fe7be2d57d
ludjoh <ludjoh734@ihmeducation.se>
2024-09-23 14:01:25 +0200

    update

--- P12Arreys/js/app.js
+++ P12Arreys/js/app.js
@@ -1,5 +1,25 @@
-let studentNames = ["Olivia", "Ruben", "James", "Liam", "Rufus", "Lucas"];
+//let studentNames = ["Olivia", "Ruben", "James", "Liam", "Rufus", "Lucas"];
 
-for (i = 0; i < studentNames.length; i++) {
-  console.log("Hejsan " +studentNames[i]+"!");
+//studentNames[2] = "Micheal";
+//studentNames.push("Ben10");
+
+//for (i = 0; i < studentNames.length; i++) {
+ // console.log("Hejsan " +studentNames[i]+"!");
+//}
+
+let randomNumberCount = [0,0,0,0,0,0,0,0,0,0];
+
+randomNumberCount[0] += 1;
+
+for (i = 0; i < 100000; i++) {
+ let randomNumber =  getRandomInt(10);
+ randomNumberCount[randomNumber] += 1;
+}
+
+for (let i = 0; i < randomNumberCount.length; i++) {
+console.log("I rolled " +i+ " " +randomNumberCount[i]+ " times");
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max)
 }
