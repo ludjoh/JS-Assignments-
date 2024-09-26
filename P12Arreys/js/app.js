576cdc6e280541669d165177ac9ca9493c6a2c68
ludjoh <ludjoh734@ihmeducation.se>
2024-09-26 09:35:15 +0200

    update

--- P12Arreys/js/app.js
+++ P12Arreys/js/app.js
@@ -7,17 +7,20 @@
  // console.log("Hejsan " +studentNames[i]+"!");
 //}
 
-let randomNumberCount = [0,0,0,0,0,0,0,0,0,0];
+//let randomNumberCount = [0,0,0,0,0,0,0,0,0,0];
 
-for (i = 0; i < 100000; i++) {
- let randomNumber =  getRandomInt(10);
- randomNumberCount[randomNumber] += 1;
-}
+//for (i = 0; i < 100000; i++) {
+// let randomNumber =  getRandomInt(10);
+ //randomNumberCount[randomNumber] += 1;
+//}
 
-for (let i = 0; i < randomNumberCount.length; i++) {
-console.log("I rolled " +i+ " " +randomNumberCount[i]+ " times");
-}
+//for (let i = 0; i < randomNumberCount.length; i++) {
+//console.log("I rolled " +i+ " " +randomNumberCount[i]+ " times");
+//}
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max)
-}
+//function getRandomInt(max) {
+ // return Math.floor(Math.random() * max)
+//}
+
+
+document.cookie = "todaysday = Thursday; expires =Thu, 29 sep 2024 12:00:00 UTC"
